c7bb22f746ef9ccdf54e1c9a94876518b6266176
rezamorsali1 <qreza118@gmail.com>
2023-10-15 07:00:48 +0330

    part14

--- ast/js/user.js
+++ ast/js/user.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {
+   
   });
+
+  const adduser = document.getElementById('add-user-buttom')
+  adduser.addEventListener('click', () => {
+    document.querySelector('.add_user_modal').classList.add('show')
+    document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+  })
+
+
+  document.querySelector('.add_user_modal_back').addEventListener('click',()=>{
+    document.querySelector('.add_user_modal').classList.remove('show')
+    document.querySelector('.add_user_modal_back').classList.add('dis-none')
+  })
+});
